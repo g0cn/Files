f4d60f6d329349ebf80d30fe7eab9c73c2a69a08
xx-xy <78575037+xx-xy@users.noreply.github.com>
2022-02-07 15:33:26 +0800

    Create change-zhihu-user-agent.js

---
+++ QX/JS/change-zhihu-user-agent.js
@@ -0,0 +1,22 @@
+#知乎直接看不需要登陆，通过改变useragent
+
+
+(function () {
+  const headers = $response.headers;
+
+  for (let key in headers) {
+    if (key.toUpperCase() === 'CONTENT-TYPE' && !headers[key].includes('text/html')) {
+      $done({});
+      return;
+    }
+    if (key.toUpperCase() === 'CONTENT-SECURITY-POLICY') {
+      delete headers[key];
+    }
+  }
+  
+  const body = $response.body.replace('<head>',
+    '<head>' +
+    '<script>delete navigator; window.navigator = { userAgent: "Mozilla/5.0 (iPad; CPU OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1" }</script>' +
+    '<style>.ContentItem-actions>*:nth-child(3),.ContentItem-actions>*:nth-child(4){display:none}</style>');
+  $done({ headers, body })
+})()
